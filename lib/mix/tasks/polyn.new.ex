b872694e0c5791f0404b6cb36e3a9296cdc27749
brandynbennett <brandynlbennett@gmail.com>
2022-06-01 16:31:47 -0600

    put steps into functions

--- lib/mix/tasks/polyn.new.ex
+++ lib/mix/tasks/polyn.new.ex
@@ -7,24 +7,49 @@ defmodule Mix.Tasks.Polyn.New do
   use Mix.Task
 
   def run(_args) do
-    Mix.Tasks.New.run(["polyn_admin"])
-    Mix.Shell.IO.cmd("cd polyn_admin && git init")
-    admin_path = Path.join(File.cwd!(), "polyn_admin")
-    File.rm_rf!(Path.join(admin_path, "test"))
-    File.rm_rf!(Path.join(admin_path, "lib"))
-    File.mkdir_p!(Path.join(admin_path, "priv/polyn/schemas"))
-    File.mkdir_p!(Path.join(admin_path, "priv/polyn/migrations"))
-    File.write!(Path.join(admin_path, "priv/polyn/schemas/.gitkeep"), "")
-    File.write!(Path.join(admin_path, "priv/polyn/migrations/.gitkeep"), "")
+    generate_new_mix_project()
+    initialize_git()
+    remove_unused()
+    create_priv_dirs()
+    copy_readme()
+    copy_tool_versions()
+  end
 
+  defp generate_new_mix_project do
+    Mix.Tasks.New.run(["polyn_admin"])
+  end
+
+  defp initialize_git do
+    Mix.Shell.IO.cmd("cd polyn_admin && git init")
+  end
+
+  defp remove_unused do
+    File.rm_rf!(Path.join(admin_path(), "test"))
+    File.rm_rf!(Path.join(admin_path(), "lib"))
+  end
+
+  defp admin_path do
+    Path.join(File.cwd!(), "polyn_admin")
+  end
+
+  defp create_priv_dirs do
+    File.mkdir_p!(Path.join(admin_path(), "priv/polyn/schemas"))
+    File.mkdir_p!(Path.join(admin_path(), "priv/polyn/migrations"))
+    File.write!(Path.join(admin_path(), "priv/polyn/schemas/.gitkeep"), "")
+    File.write!(Path.join(admin_path(), "priv/polyn/migrations/.gitkeep"), "")
+  end
+
+  defp copy_readme do
     File.copy!(
       Path.join(File.cwd!(), "priv/templates/README.md"),
-      Path.join(admin_path, "README.md")
+      Path.join(admin_path(), "README.md")
     )
+  end
 
+  defp copy_tool_versions do
     File.copy!(
       Path.join(File.cwd!(), ".tool-versions"),
-      Path.join(admin_path, ".tool-versions")
+      Path.join(admin_path(), ".tool-versions")
     )
   end
 end
